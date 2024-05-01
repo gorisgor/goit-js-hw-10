bb123cff5f4734c386826c2f4d0c543fcb4b2299
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-01 08:00:15 +0300

    jsupdate

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,11 @@
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+
+
+
+
+iziToast.show({
+    title: 'Hey',
+    message: 'What would you like to add?'
+});
