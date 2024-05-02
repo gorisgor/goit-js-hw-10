3f82e95d917da14b97468e947006ff6186217920
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-02 09:53:54 +0300

    usnak

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,12 +1,54 @@
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
+const refs = {
+    delayInput: document.querySelector("input[name='delay']"),
+    checkboxState: document.querySelectorAll("input[name='state']"),
+    submitForm: document.querySelector(".form") 
+};
 
-
-
-iziToast.show({
-    title: 'Hey',
-    message: 'What would you like to add?'
+let delayTime = 0;
+refs.delayInput.addEventListener("change", () => {
+    delayTime = refs.delayInput.value;
 });
+
+let state = [];
+const checkboxArray = Array.from(refs.checkboxState);
+checkboxArray.forEach(checkbox => {
+    checkbox.addEventListener("change", onCheckbox);
+});
+
+function onCheckbox() {
+    state = checkboxArray.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+}
+
+refs.submitForm.addEventListener("submit", onSubmit); 
+function onSubmit(event) {
+    console.log(state);
+    console.log(delayTime);
+    event.preventDefault();
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                    if (state.includes('fulfilled')) {
+                        resolve(`✅ Fulfilled promise in ${delayTime}ms`);
+                    } else if (state.includes('rejected')) {
+                        reject(`❌ Rejected promise in ${delayTime}ms`);
+                    }
+            }, delayTime);
+        });
+        promise.then(message => {
+            iziToast.show({
+                message: message,
+                messageColor: '#fff',
+                backgroundColor: '#59a10d',
+                position: 'topRight',
+            });
+        }).catch(error => {
+            iziToast.show({
+                message: error,
+                messageColor: '#fff',
+                backgroundColor: '#ef4040',
+                position: 'topRight',
+            })
+        });
+}
