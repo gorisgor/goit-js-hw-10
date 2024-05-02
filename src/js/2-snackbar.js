f9327f5ca094e5c11df4a4adf128f7056495112a
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-05-02 11:12:34 +0300

    u

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -31,20 +31,23 @@ function onSubmit(event) {
     });
 
     let selectedState = '';
+    let selectedDelayTime = 0;
     if (state.includes('fulfilled')) {
         selectedState = 'fulfilled';
+        selectedDelayTime = delayTime;
     } else if (state.includes('rejected')) {
         selectedState = 'rejected';
+        selectedDelayTime = delayTime;
     }
 
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
             if (selectedState === 'fulfilled') {
-                resolve(`✅ Fulfilled promise in ${delayTime}ms`);
+                resolve(`✅ Fulfilled promise in ${selectedDelayTime}ms`);
             } else if (selectedState === 'rejected') {
-                reject(`❌ Rejected promise in ${delayTime}ms`);
+                reject(`❌ Rejected promise in ${selectedDelayTime}ms`);
             }
-        }, delayTime);
+        }, selectedDelayTime);
     });
         promise.then(message => {
             iziToast.show({
