d8461e352efcbb5e27f0dd1449566cfb297896a2
gorisgor <igor.matvienko1989@gmail.com>
2024-05-03 11:15:46 +0300

    usnack

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -7,89 +7,36 @@ const refs = {
 };
 
 refs.submitBtn.addEventListener('click', onSubmit);
-
 function onSubmit(event) {
   event.preventDefault();
   const delay = refs.form.elements.delay.value;
-  console.log(delay);
   const state = refs.form.elements.state.value;
-  console.log(state);
   const promise = new Promise((resolve, reject) => {
-    if (state === 'fulfiled') {
+    if (state === 'fulfilled') {
       setTimeout(() => resolve(delay), delay);
     } else if (state === 'rejected') {
       setTimeout(() => reject(delay), delay);
     }
   });
-  promise.then;
+  promise.then(
+    delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#59a10d',
+        position: 'topRight',
+        progressBar: false,
+      });
+    },
+    delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+        progressBar: false,
+      });
+    }
+  );
+  refs.form.reset();
 }
-
-// const refs = {
-//   delayInput: document.querySelector("input[name='delay']"),
-//   checkboxState: document.querySelectorAll("input[name='state']"),
-//   submitForm: document.querySelector('.form'),
-// };
-
-// let delayTime = 0;
-// refs.delayInput.addEventListener('change', () => {
-//   delayTime = refs.delayInput.value;
-// });
-
-// let state = [];
-// const checkboxArray = Array.from(refs.checkboxState);
-// checkboxArray.forEach(checkbox => {
-//   checkbox.addEventListener('change', onCheckbox);
-// });
-
-// function onCheckbox() {
-//   state = checkboxArray
-//     .filter(checkbox => checkbox.checked)
-//     .map(checkbox => checkbox.value);
-// }
-
-// refs.submitForm.addEventListener('submit', onSubmit);
-// function onSubmit(event) {
-//   event.preventDefault();
-//   refs.delayInput.value = '';
-//   checkboxArray.forEach(checkbox => {
-//     checkbox.checked = false;
-//   });
-//   let selectedState = '';
-//   let selectedDelayTime = 0;
-//   if (state.includes('fulfilled')) {
-//     selectedState = 'fulfilled';
-//     selectedDelayTime = delayTime;
-//   } else if (state.includes('rejected')) {
-//     selectedState = 'rejected';
-//     selectedDelayTime = delayTime;
-//   }
-
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (selectedState === 'fulfilled') {
-//         resolve(`✅ Fulfilled promise in ${selectedDelayTime}ms`);
-//       } else if (selectedState === 'rejected') {
-//         reject(`❌ Rejected promise in ${selectedDelayTime}ms`);
-//       }
-//     }, selectedDelayTime);
-//   });
-//   promise
-//     .then(message => {
-//       iziToast.show({
-//         message: message,
-//         messageColor: '#fff',
-//         backgroundColor: '#59a10d',
-//         position: 'topRight',
-//         progressBar: false,
-//       });
-//     })
-//     .catch(error => {
-//       iziToast.show({
-//         message: error,
-//         messageColor: '#fff',
-//         backgroundColor: '#ef4040',
-//         position: 'topRight',
-//         progressBar: false,
-//       });
-//     });
-// }
