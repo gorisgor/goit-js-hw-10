d20e69be0d8c1268a30638de5dd730761b251751
gorisgor <igor.matvienko1989@gmail.com>
2024-05-03 09:15:47 +0300

    usnack

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,90 +3,88 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-
 const refs = {
-  calendar: document.querySelector("#datetime-picker"),
-  btn: document.querySelector("[data-start]"),
-  days: document.querySelector("[data-days]"),
-  hours: document.querySelector("[data-hours]"),
-  minutes: document.querySelector("[data-minutes]"),
-  seconds: document.querySelector("[data-seconds]"),
-}
+  calendar: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
 
 let intervalId = null;
 let userSelectedDate = null;
-
+refs.btn.disabled = true;
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-    if (selectedDates[0] > Date.now()){
-        refs.btn.disabled = false;
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] > Date.now()) {
+      refs.btn.disabled = false;
       userSelectedDate = selectedDates[0];
-          } else {
-            refs.btn.disabled = true;
-            iziToast.show({
-              message: 'Please choose a date in the future',
-              messageColor: '#fff',
-              backgroundColor: '#ef4040',
-              position: 'topRight',
-              });
-            }
-    }, 
-  };
+    } else {
+      refs.btn.disabled = true;
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+      });
+    }
+  },
+};
 flatpickr(refs.calendar, options);
 
 const timer = {
-  start(){
-const endTime = userSelectedDate;
-intervalId = setInterval(()=>{
-  const currentTime = Date.now();
-  const deltaTime = endTime - currentTime;
-  refs.btn.disabled = true;
-  refs.calendar.disabled = true;
-    if (deltaTime <= 0) {
-      clearInterval(intervalId);
-      updateClockface({ days: '00', hours: '00', minutes: '00', seconds: '00' });
-      refs.btn.disabled = false;
-      refs.calendar.disabled = false;
-    } else {
-      const time = convertMs(deltaTime);
-      updateClockface(time);
+  start() {
+    const endTime = userSelectedDate;
+    intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = endTime - currentTime;
+      refs.btn.disabled = true;
+      refs.calendar.disabled = true;
+      if (deltaTime <= 0) {
+        clearInterval(intervalId);
+        updateClockface({
+          days: '00',
+          hours: '00',
+          minutes: '00',
+          seconds: '00',
+        });
+        refs.btn.disabled = false;
+        refs.calendar.disabled = false;
+      } else {
+        const time = convertMs(deltaTime);
+        updateClockface(time);
       }
     }, 1000);
-  }
-}
+  },
+};
 
 refs.btn.addEventListener('click', () => {
-    timer.start()
-  } 
-)
+  timer.start();
+});
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24; 
-    const days = pad(Math.floor(ms / day));
-    const hours = pad(Math.floor((ms % day) / hour));
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-    return { days, hours, minutes, seconds };
-  }
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
+}
 
-  function pad(value) {
-    return String(value).padStart(2, '0');
-  }
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-  function updateClockface({ days, hours, minutes, seconds }) {
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.minutes.textContent = `${minutes}`;
-    refs.seconds.textContent = `${seconds}`;
-  }
-
-// iziToast.show({
-//   title: 'Hey',
-//   message: 'What would you like to add?'
-// });
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,71 +2,94 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
-  delayInput: document.querySelector("input[name='delay']"),
-  checkboxState: document.querySelectorAll("input[name='state']"),
-  submitForm: document.querySelector('.form'),
+  form: document.querySelector('.form'),
+  submitBtn: document.querySelector('button[type="submit"]'),
 };
 
-let delayTime = 0;
-refs.delayInput.addEventListener('change', () => {
-  delayTime = refs.delayInput.value;
-});
+refs.submitBtn.addEventListener('click', onSubmit);
 
-let state = [];
-const checkboxArray = Array.from(refs.checkboxState);
-checkboxArray.forEach(checkbox => {
-  checkbox.addEventListener('change', onCheckbox);
-});
-
-function onCheckbox() {
-  state = checkboxArray
-    .filter(checkbox => checkbox.checked)
-    .map(checkbox => checkbox.value);
-}
-
-refs.submitForm.addEventListener('submit', onSubmit);
 function onSubmit(event) {
   event.preventDefault();
-  refs.delayInput.value = '';
-  checkboxArray.forEach(checkbox => {
-    checkbox.checked = false;
-  });
-  let selectedState = '';
-  let selectedDelayTime = 0;
-  if (state.includes('fulfilled')) {
-    selectedState = 'fulfilled';
-    selectedDelayTime = delayTime;
-  } else if (state.includes('rejected')) {
-    selectedState = 'rejected';
-    selectedDelayTime = delayTime;
-  }
-
+  const delay = refs.form.elements.delay.value;
+  console.log(delay);
+  const state = refs.form.elements.state.value;
+  console.log(state);
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (selectedState === 'fulfilled') {
-        resolve(`✅ Fulfilled promise in ${selectedDelayTime}ms`);
-      } else if (selectedState === 'rejected') {
-        reject(`❌ Rejected promise in ${selectedDelayTime}ms`);
-      }
-    }, selectedDelayTime);
+    if (state === 'fulfiled') {
+      setTimeout(() => resolve(delay), delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => reject(delay), delay);
+    }
   });
-  promise
-    .then(message => {
-      iziToast.show({
-        message: message,
-        messageColor: '#fff',
-        backgroundColor: '#59a10d',
-        position: 'topRight',
-        progressBar: false,
-      });
-    })
-    .catch(error => {
-      iziToast.show({
-        message: error,
-        messageColor: '#fff',
-        backgroundColor: '#ef4040',
-        position: 'topRight',
-        progressBar: false,
-      });
-    });
+  promise.then;
 }
+
+// const refs = {
+//   delayInput: document.querySelector("input[name='delay']"),
+//   checkboxState: document.querySelectorAll("input[name='state']"),
+//   submitForm: document.querySelector('.form'),
+// };
+
+// let delayTime = 0;
+// refs.delayInput.addEventListener('change', () => {
+//   delayTime = refs.delayInput.value;
+// });
+
+// let state = [];
+// const checkboxArray = Array.from(refs.checkboxState);
+// checkboxArray.forEach(checkbox => {
+//   checkbox.addEventListener('change', onCheckbox);
+// });
+
+// function onCheckbox() {
+//   state = checkboxArray
+//     .filter(checkbox => checkbox.checked)
+//     .map(checkbox => checkbox.value);
+// }
+
+// refs.submitForm.addEventListener('submit', onSubmit);
+// function onSubmit(event) {
+//   event.preventDefault();
+//   refs.delayInput.value = '';
+//   checkboxArray.forEach(checkbox => {
+//     checkbox.checked = false;
+//   });
+//   let selectedState = '';
+//   let selectedDelayTime = 0;
+//   if (state.includes('fulfilled')) {
+//     selectedState = 'fulfilled';
+//     selectedDelayTime = delayTime;
+//   } else if (state.includes('rejected')) {
+//     selectedState = 'rejected';
+//     selectedDelayTime = delayTime;
+//   }
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (selectedState === 'fulfilled') {
+//         resolve(`✅ Fulfilled promise in ${selectedDelayTime}ms`);
+//       } else if (selectedState === 'rejected') {
+//         reject(`❌ Rejected promise in ${selectedDelayTime}ms`);
+//       }
+//     }, selectedDelayTime);
+//   });
+//   promise
+//     .then(message => {
+//       iziToast.show({
+//         message: message,
+//         messageColor: '#fff',
+//         backgroundColor: '#59a10d',
+//         position: 'topRight',
+//         progressBar: false,
+//       });
+//     })
+//     .catch(error => {
+//       iziToast.show({
+//         message: error,
+//         messageColor: '#fff',
+//         backgroundColor: '#ef4040',
+//         position: 'topRight',
+//         progressBar: false,
+//       });
+//     });
+// }
